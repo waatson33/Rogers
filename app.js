1f862c301957230285e112fa400765d9daf7087a
waatson33 <146733190+waatson33@users.noreply.github.com>
2023-10-02 07:35:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_gc1g7n4", "template_6h37nbh", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
